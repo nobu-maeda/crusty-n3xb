3643709d00b9e0011faf8e1382bea1ae50526ffe
Nobu Maeda <nobu.maeda@proton.me>
2023-07-25 22:48:22 -0400

    Implement ObligationKind from String

--- src/order/obligation.rs
+++ src/order/obligation.rs
@@ -1,6 +1,9 @@
 use super::types::*;
+use crate::error::N3xbError;
 use iso_currency::Currency;
 use serde::{Deserialize, Serialize};
+use std::result::Result;
+use std::str::FromStr;
 use std::{collections::HashSet, fmt::*};
 
 #[derive(Clone, Debug)]
@@ -35,48 +38,131 @@ pub enum ObligationKind {
     Custom(String),
 }
 
+const OBLIGATION_KIND_PREFIX_BITCOIN: &str = "bitcoin";
+const OBLIGATION_KIND_PREFIX_FIAT: &str = "fiat";
+const OBLIGATION_KIND_PREFIX_CUSTOM: &str = "custom";
+const OBLIGATION_KIND_SPLIT_CHAR: &str = "-";
+
 impl ObligationKind {
     pub fn to_tags(&self) -> HashSet<String> {
         let mut tag_string_set: HashSet<String>;
         match self {
             ObligationKind::Bitcoin(settlement_methods) => {
-                let prefix_string = "ob-bitcoin";
+                let prefix_string = OBLIGATION_KIND_PREFIX_BITCOIN;
                 tag_string_set = HashSet::from([prefix_string.to_string()]);
                 for settlment_method in settlement_methods {
-                    let tag_string = format!("{}-{}", prefix_string, settlment_method.to_string());
-                    tag_string_set.insert(tag_string.to_lowercase());
+                    let tag_string = format!(
+                        "{}{}{}",
+                        prefix_string,
+                        OBLIGATION_KIND_SPLIT_CHAR,
+                        settlment_method.to_string()
+                    );
+                    tag_string_set.insert(tag_string);
                 }
             }
 
             ObligationKind::Fiat(currency, payment_methods) => {
-                let prefix_string = "ob-fiat";
-                let currency_prefix_string =
-                    format!("{}-{}", prefix_string, currency.code().to_string());
-                tag_string_set = HashSet::from([
-                    prefix_string.to_string(),
-                    currency_prefix_string.to_lowercase(),
-                ]);
+                let prefix_string = OBLIGATION_KIND_PREFIX_FIAT;
+                let currency_prefix_string = format!(
+                    "{}{}{}",
+                    prefix_string,
+                    OBLIGATION_KIND_SPLIT_CHAR,
+                    currency.code().to_string()
+                );
+                tag_string_set =
+                    HashSet::from([prefix_string.to_string(), currency_prefix_string.clone()]);
                 for payment_method in payment_methods {
-                    let tag_string =
-                        format!("{}-{}", currency_prefix_string, payment_method.to_string());
-                    tag_string_set.insert(tag_string.to_lowercase());
+                    let tag_string = format!(
+                        "{}{}{}",
+                        currency_prefix_string,
+                        OBLIGATION_KIND_SPLIT_CHAR,
+                        payment_method.to_string()
+                    );
+                    tag_string_set.insert(tag_string);
                 }
             }
 
             ObligationKind::Custom(obligation_string) => {
-                let prefix_string = "ob-custom";
+                let prefix_string = OBLIGATION_KIND_PREFIX_CUSTOM;
                 tag_string_set = HashSet::from([
                     prefix_string.to_string(),
-                    format!("{}-{}", prefix_string, obligation_string).to_lowercase(),
+                    format!(
+                        "{}{}{}",
+                        prefix_string, OBLIGATION_KIND_SPLIT_CHAR, obligation_string
+                    ),
                 ]);
             }
         }
         tag_string_set
     }
+
+    pub fn from_tags(tags: HashSet<String>) -> Result<ObligationKind, N3xbError> {
+        let obligation_kind_prefix_set: HashSet<&str> = HashSet::from([
+            OBLIGATION_KIND_PREFIX_BITCOIN,
+            OBLIGATION_KIND_PREFIX_FIAT,
+            OBLIGATION_KIND_PREFIX_CUSTOM,
+        ]);
+
+        let mut kind_prefix: Option<String> = None;
+        let mut currency: Option<Currency> = None;
+        let mut bitcoin_methods: HashSet<BitcoinSettlementMethod> = HashSet::new();
+        let mut fiat_methods: HashSet<FiatPaymentMethod> = HashSet::new();
+        let mut custom_ob_string: Option<String> = None;
+
+        for tag in tags.clone() {
+            let splits_set: Vec<&str> = tag.split(OBLIGATION_KIND_SPLIT_CHAR).collect();
+
+            let splits_prefix = splits_set[0];
+            if !obligation_kind_prefix_set.contains(splits_prefix) {
+                return Err(N3xbError::Other("Unrecgonized Prefix".to_string()));
+            } else if let Some(kind_prefix_unwrapped) = kind_prefix.clone() {
+                if kind_prefix_unwrapped != splits_prefix {
+                    let err_string = format!(
+                        "Obligation tag set contains contradictory prefixes\n {:?}",
+                        tags
+                    );
+                    return Err(N3xbError::Other(err_string));
+                }
+            } else {
+                kind_prefix = Some(splits_prefix.to_string());
+            }
+
+            if OBLIGATION_KIND_PREFIX_BITCOIN == kind_prefix.as_ref().unwrap() {
+                if splits_set.len() > 1 {
+                    let bitcoin_method = BitcoinSettlementMethod::from_str(splits_set[1]).unwrap();
+                    bitcoin_methods.insert(bitcoin_method);
+                }
+            } else if OBLIGATION_KIND_PREFIX_FIAT == kind_prefix.as_ref().unwrap() {
+                if splits_set.len() > 1 {
+                    currency = Some(Currency::from_str(splits_set[1]).unwrap());
+                }
+                if splits_set.len() > 2 {
+                    let fiat_method = FiatPaymentMethod::from_str(splits_set[2]).unwrap();
+                    fiat_methods.insert(fiat_method);
+                }
+            } else if OBLIGATION_KIND_PREFIX_CUSTOM == kind_prefix.as_ref().unwrap() {
+                if splits_set.len() > 1 {
+                    custom_ob_string = Some(splits_set[1].to_string());
+                }
+            } else {
+                return Err(N3xbError::Other("Unexpected Prefix".to_string()));
+            }
+        }
+
+        if OBLIGATION_KIND_PREFIX_BITCOIN == kind_prefix.as_ref().unwrap() {
+            return Ok(ObligationKind::Bitcoin(bitcoin_methods));
+        } else if OBLIGATION_KIND_PREFIX_FIAT == kind_prefix.as_ref().unwrap() {
+            return Ok(ObligationKind::Fiat(currency.unwrap(), fiat_methods));
+        } else if OBLIGATION_KIND_PREFIX_CUSTOM == kind_prefix.as_ref().unwrap() {
+            return Ok(ObligationKind::Custom(custom_ob_string.unwrap()));
+        } else {
+            return Err(N3xbError::Other("Unexpected Obligation Kind".to_string()));
+        }
+    }
 }
 
 impl Display for ObligationKind {
-    fn fmt(&self, f: &mut Formatter) -> Result {
+    fn fmt(&self, f: &mut Formatter) -> core::fmt::Result {
         match self {
             ObligationKind::Bitcoin(settlement_methods) => {
                 write!(f, "Bitcoin {:?} Settlements", settlement_methods)
@@ -100,13 +186,25 @@ mod tests {
         let obligation_kind =
             ObligationKind::Bitcoin(HashSet::from([BitcoinSettlementMethod::Onchain]));
         let obligation_tags = obligation_kind.to_tags();
-        let expected_tags =
-            HashSet::from(["ob-bitcoin-onchain".to_string(), "ob-bitcoin".to_string()]);
+        let expected_tags = HashSet::from(["bitcoin-onchain".to_string(), "bitcoin".to_string()]);
         print!(
             "Obligation: {:?} Expected: {:?}",
             obligation_tags, expected_tags
         );
         assert_eq!(obligation_tags, expected_tags);
+    }
+
+    #[test]
+    fn bitcoin_onchain_obligation_kind_from_tags() {
+        let obligation_tags = HashSet::from(["bitcoin-onchain".to_string(), "bitcoin".to_string()]);
+        let obligation_kind = ObligationKind::from_tags(obligation_tags).unwrap();
+        let expected_kind =
+            ObligationKind::Bitcoin(HashSet::from([BitcoinSettlementMethod::Onchain]));
+        print!(
+            "Obligation Kind: {:?} Expected: {:?}",
+            obligation_kind, expected_kind
+        );
+        assert_eq!(obligation_kind, expected_kind);
     }
 
     #[test]
@@ -117,10 +215,10 @@ mod tests {
         );
         let obligation_tags = obligation_kind.to_tags();
         let expected_tags = HashSet::from([
-            "ob-fiat-usd-venmo".to_string(),
-            "ob-fiat-usd-cashapp".to_string(),
-            "ob-fiat-usd".to_string(),
-            "ob-fiat".to_string(),
+            "fiat-USD-venmo".to_string(),
+            "fiat-USD-cashapp".to_string(),
+            "fiat-USD".to_string(),
+            "fiat".to_string(),
         ]);
         print!(
             "Obligation: {:?} Expected: {:?}",
@@ -130,15 +228,46 @@ mod tests {
     }
 
     #[test]
+    fn fiat_usd_venmo_obligation_kind_from_tags() {
+        let obligation_tags = HashSet::from([
+            "fiat-USD-venmo".to_string(),
+            "fiat-USD-cashapp".to_string(),
+            "fiat-USD".to_string(),
+            "fiat".to_string(),
+        ]);
+        let obligation_kind = ObligationKind::from_tags(obligation_tags).unwrap();
+        let expected_kind = ObligationKind::Fiat(
+            Currency::USD,
+            HashSet::from([FiatPaymentMethod::Venmo, FiatPaymentMethod::CashApp]),
+        );
+        print!(
+            "Obligation: {:?} Expected: {:?}",
+            obligation_kind, expected_kind
+        );
+        assert_eq!(obligation_kind, expected_kind);
+    }
+
+    #[test]
     fn custom_obligation_kind_to_tags() {
         let obligation_kind = ObligationKind::Custom("barter".to_string());
         let obligation_tags = obligation_kind.to_tags();
-        let expected_tags =
-            HashSet::from(["ob-custom-barter".to_string(), "ob-custom".to_string()]);
+        let expected_tags = HashSet::from(["custom-barter".to_string(), "custom".to_string()]);
         print!(
             "Obligation: {:?} Expected: {:?}",
             obligation_tags, expected_tags
         );
         assert_eq!(obligation_tags, expected_tags);
+    }
+
+    #[test]
+    fn custom_obligation_kind_from_tags() {
+        let obligation_tags = HashSet::from(["custom-barter".to_string(), "custom".to_string()]);
+        let obligation_kind = ObligationKind::from_tags(obligation_tags).unwrap();
+        let expected_kind = ObligationKind::Custom("barter".to_string());
+        print!(
+            "Obligation: {:?} Expected: {:?}",
+            obligation_kind, expected_kind
+        );
+        assert_eq!(obligation_kind, expected_kind);
     }
 }
--- src/order/types.rs
+++ src/order/types.rs
@@ -1,9 +1,9 @@
 use serde::{Deserialize, Serialize};
 use std::any::Any;
 use std::collections::HashSet;
-use std::fmt::Result;
-use std::fmt::*;
+use std::fmt::{Display, Formatter, Result};
 use std::hash::Hash;
+use strum_macros::{Display, EnumString, IntoStaticStr};
 
 pub enum BuySell {
     Buy,
@@ -55,26 +55,21 @@ impl Display for EventKind {
 
 pub static N3XB_APPLICATION_TAG: &str = "n3xb";
 
-#[derive(PartialEq, Eq, Hash, Clone, Debug)]
+#[derive(PartialEq, Eq, Hash, Clone, Debug, EnumString, Display, IntoStaticStr)]
+#[strum(serialize_all = "lowercase")]
 pub enum BitcoinSettlementMethod {
     Onchain,
     Lightning,
-}
-
-impl Display for BitcoinSettlementMethod {
-    fn fmt(&self, f: &mut Formatter) -> Result {
-        match self {
-            BitcoinSettlementMethod::Onchain => write!(f, "Onchain"),
-            BitcoinSettlementMethod::Lightning => write!(f, "Lightning"),
-        }
-    }
 }
 
 // List of fiat payment methods from
 // https://github.com/bisq-network/bisq/blob/release/v1.9.10/core/src/main/java/bisq/core/payment/payload/PaymentMethod.java
 // We are not implementing trade limits and risk association here. This should be for the higher level to determine.
 
-#[derive(PartialEq, Eq, Hash, Clone, Debug, Serialize, Deserialize)]
+#[derive(
+    PartialEq, Eq, Hash, Clone, Debug, Serialize, Deserialize, EnumString, Display, IntoStaticStr,
+)]
+#[strum(serialize_all = "lowercase")]
 pub enum FiatPaymentMethod {
     Uphold,
     MoneyBeam,
@@ -133,61 +128,4 @@ pub enum FiatPaymentMethod {
     OkPay,
     CashApp,
     Venmo,
-}
-
-impl Display for FiatPaymentMethod {
-    fn fmt(&self, f: &mut Formatter) -> Result {
-        match self {
-            FiatPaymentMethod::Uphold => write!(f, "Uphold"),
-            FiatPaymentMethod::MoneyBeam => write!(f, "MoneyBeam"),
-            FiatPaymentMethod::PopMoney => write!(f, "PopMoney"),
-            FiatPaymentMethod::Revolut => write!(f, "Revolut"),
-            FiatPaymentMethod::PerfectMoney => write!(f, "PerfectMoney"),
-            FiatPaymentMethod::Sepa => write!(f, "SEPA"),
-            FiatPaymentMethod::SepaInstant => write!(f, "SEPAInstant"),
-            FiatPaymentMethod::FasterPayments => write!(f, "FasterPayments"),
-            FiatPaymentMethod::NationalBank => write!(f, "NationalBank"),
-            FiatPaymentMethod::JapanBank => write!(f, "JapanBank"),
-            FiatPaymentMethod::AustraliaPayID => write!(f, "AustraliaPayID"),
-            FiatPaymentMethod::Swish => write!(f, "Swish"),
-            FiatPaymentMethod::AliPay => write!(f, "AliPay"),
-            FiatPaymentMethod::WeChatPay => write!(f, "WeChatPay"),
-            FiatPaymentMethod::Zelle => write!(f, "Zelle"),
-            FiatPaymentMethod::InteracETransfer => write!(f, "InteracETransfer"),
-            FiatPaymentMethod::USPostalMoneyOrder => write!(f, "USPostalMoneyOrder"),
-            FiatPaymentMethod::CashDeposit => write!(f, "CashDeposit"),
-            FiatPaymentMethod::MoneyGram => write!(f, "MoneyGram"),
-            FiatPaymentMethod::WesternUnion => write!(f, "WesternUnion"),
-            FiatPaymentMethod::FaceToFace => write!(f, "FaceToFace"),
-            FiatPaymentMethod::HalCash => write!(f, "HalCash"),
-            FiatPaymentMethod::PromptPay => write!(f, "PromptPay"),
-            FiatPaymentMethod::AdvancedCash => write!(f, "AdvancedCash"),
-            FiatPaymentMethod::TransferWise => write!(f, "TransferWise"),
-            FiatPaymentMethod::Paysera => write!(f, "Paysera"),
-            FiatPaymentMethod::Paxum => write!(f, "Paxum"),
-            FiatPaymentMethod::NEFT => write!(f, "NEFT"),
-            FiatPaymentMethod::RTGS => write!(f, "RTGS"),
-            FiatPaymentMethod::IMPS => write!(f, "IMPS"),
-            FiatPaymentMethod::UPI => write!(f, "UPI"),
-            FiatPaymentMethod::Paytm => write!(f, "Paytm"),
-            FiatPaymentMethod::Nequi => write!(f, "Nequi"),
-            FiatPaymentMethod::Bizum => write!(f, "Bizum"),
-            FiatPaymentMethod::Pix => write!(f, "Pix"),
-            FiatPaymentMethod::AmazonGiftCard => write!(f, "AmazonGiftCard"),
-            FiatPaymentMethod::CashByMail => write!(f, "CashByMail"),
-            FiatPaymentMethod::Capitual => write!(f, "Capitual"),
-            FiatPaymentMethod::Celpay => write!(f, "Celpay"),
-            FiatPaymentMethod::Monese => write!(f, "Monese"),
-            FiatPaymentMethod::Satispay => write!(f, "Satispay"),
-            FiatPaymentMethod::Tikkie => write!(f, "Tikkie"),
-            FiatPaymentMethod::Verse => write!(f, "Verse"),
-            FiatPaymentMethod::Strike => write!(f, "Strike"),
-            FiatPaymentMethod::SWIFT => write!(f, "SWIFT"),
-            FiatPaymentMethod::ACHTransfer => write!(f, "ACHTransfer"),
-            FiatPaymentMethod::DomesticWireTransfer => write!(f, "DomesticWireTransfer"),
-            FiatPaymentMethod::OkPay => write!(f, "OkPay"),
-            FiatPaymentMethod::CashApp => write!(f, "CashApp"),
-            FiatPaymentMethod::Venmo => write!(f, "Venmo"),
-        }
-    }
 }
