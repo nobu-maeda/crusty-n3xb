3e4b7eec103d0573c3d626a0bde6f85dff069652
Nobu Maeda <nobu.maeda@proton.me>
2023-11-14 23:18:54 +0800

    Clean up Test and Test Relay dependencies

--- tests/tests.rs
+++ tests/tests.rs
@@ -2,41 +2,68 @@ mod common;
 
 #[cfg(test)]
 mod integration_tests {
+    use std::net::SocketAddr;
+    use std::time::Duration;
+
     use crusty_n3xb::manager::Manager;
     use crusty_n3xb::testing::*;
+    use tokio::time::sleep;
 
     use super::common::maker_simple_tester::MakerSimpleTester;
     use super::common::relay::Relay;
     use super::common::taker_simple_tester::TakerSimpleTester;
 
     #[tokio::test]
-    async fn test_simple_full_flow() {
+    async fn test_simple_four_relays_flow() {
         // setup tracing
         let _trace_sub = tracing_subscriber::fmt::try_init();
 
-        let relay = Relay::start();
+        let mut relays: Vec<Relay> = Vec::new();
+
+        let relay: Relay = Relay::start();
         relay.wait_for_healthy_relay().await.unwrap();
+        relays.push(relay);
+
+        let relay: Relay = Relay::start();
+        relay.wait_for_healthy_relay().await.unwrap();
+        relays.push(relay);
+
+        let relay: Relay = Relay::start();
+        relay.wait_for_healthy_relay().await.unwrap();
+        relays.push(relay);
+
+        let relay: Relay = Relay::start();
+        relay.wait_for_healthy_relay().await.unwrap();
+        relays.push(relay);
 
         let test_engine_name = SomeTestParams::engine_name_str();
         let maker_manager = Manager::new(&test_engine_name).await;
         let taker_manager = Manager::new(&test_engine_name).await;
 
-        let relays = vec![(format!("{}:{}", "ws://localhost", relay.port), None)];
+        let mut relay_addrs: Vec<(String, Option<SocketAddr>)> = Vec::new();
+
+        for relay in relays.iter_mut() {
+            relay_addrs.push((format!("{}:{}", "ws://localhost", relay.port), None));
+        }
+
         maker_manager
-            .add_relays(relays.clone(), true)
+            .add_relays(relay_addrs.clone(), true)
             .await
             .unwrap();
-        taker_manager.add_relays(relays, true).await.unwrap();
+        taker_manager.add_relays(relay_addrs, true).await.unwrap();
 
         let order = SomeTestOrderParams::default_builder().build().unwrap();
         let trade_uuid = order.trade_uuid.clone();
 
         let maker_tester = MakerSimpleTester::start(maker_manager, order).await;
+
+        sleep(Duration::from_secs(1)).await; // Wait for all 4 relays to have the order
+
         let taker_tester = TakerSimpleTester::start(taker_manager, trade_uuid).await;
 
         maker_tester.wait_for_completion().await.unwrap();
         taker_tester.wait_for_completion().await.unwrap();
 
-        relay.shutdown().unwrap();
+        relays.into_iter().for_each(|r| r.shutdown().unwrap());
     }
 }
